954b1abaafbc512468df9d5d50e373237ac62426
Mishanicho <misha.teunisse@gmail.com>
2023-09-04 16:37:22 +0200

    Once again

--- challenge_day1/misha.py
+++ challenge_day1/misha.py
@@ -1,10 +1,9 @@
-'''
 from Alicja import Jack
 from ella import Jenny
 from Vanessa import Jane
 from Cristian import John
 from Ioanna import Janine
-''' 
+
 
 def my_name():
     name= "Misha"
@@ -13,7 +12,7 @@ def my_name():
 
 def Jeremy():
    return "Jeremy"
-'''
+
 
 def paragraph_five():
     name1 = jack()
@@ -22,13 +21,28 @@ def paragraph_five():
     name4 = John()
     name5 = Janine()
     story =  'f/ On a warm Friday in the summer, things didnt go so well: {name1}   broke his leg. {name4} lost his mum. {name2} went through her back. {name5} could not be seen. Jeremy had to go to therapy. And {name3} fell from a plane. Quite a bad day.'
+    return story
+print(paragraph_five())
 
 def paragraph_ten():
-    story2 = f" On a Thursday in the autumn the characters tried each other’s instruments. " import Jenny " played the trumpet. " import Jack " played the piano. " import Janine " played the trombone. Jeremy played the violin. " import Jane " played the cello. " import John " played the saxophone. The characters had great fun trying each other’s instruments.
+    name1 = jack()
+    name2 = Jenny()
+    name3 = Jane()
+    name4 = John()
+    name5 = Janine()
+    story2 = 'f/ On a Thursday in the autumn the characters tried each other’s instruments. {name2} played the trumpet. {name1} played the piano. {name5} played the trombone. Jeremy played the violin. {name3} played the cello. {name4} played the saxophone. The characters had great fun trying each other’s instruments. '
+    return story2
+print(paragraph_ten())
     
 def paragraph_fiveteen():
-    story3 = f" On Wednesday "import John " ate some oranges. " import Janine " decided to bake some cookies. Jeremy ate some nice french fries. " import Jane " had some nice cola. " import Jack " ate a good apple. " import Jenny " had some expensive lobster.
+    name1 = jack()
+    name2 = Jenny()
+    name3 = Jane()
+    name4 = John()
+    name5 = Janine()
+    story3 = 'f On Wednesday {name4} ate some oranges. {name5} decided to bake some cookies. Jeremy ate some nice french fries. {name3} had some nice cola. {name1} ate a good apple. {name2} had some expensive lobster. '
+    return story3
+print(paragraph_fiveteen())
 
 
-'''
 
