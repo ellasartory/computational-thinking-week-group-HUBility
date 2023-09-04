f50d032a1b67a85497078538d4eb64030b35f0da
vanessafigini8 <vanessa.figini@student.uva.nl>
2023-09-04 17:28:58 +0200

    Finish

--- challenge_day1/Vanessa.py
+++ challenge_day1/Vanessa.py
@@ -6,9 +6,8 @@ noun= my_name()
 
 
 def Jane():
-    n="Jane"
-    return(n)
-j= story_name()
+    return "Jane"
+
 
 
 
@@ -29,7 +28,7 @@ def paragraph6():
 
     story= f"On Saturday {name_Jenny} went out to party. {name_Jack} met a friend.{name_Janine} built a treehouse.      {name_John} worked at a cafe.{name_Jeremy} babysat his little sibling.{name_Jane} went or vacaton."
     
-    return(story)
+    return story
 
 def paragraph11():
     
@@ -42,7 +41,7 @@ def paragraph11():
 
     story_1= f"On a cold Friday in autumn, things were looking bleak:{name_John} broke his leg.{name_Jenny} lost her     mum. went through her back. {name_Jeremy} could not be seen.{name_Jane} had to go to therapy.And {name_Jack}         fell from a plane.{name_Janine}Quite a bar dey."
     
-    return(story_1)
+    return story_1
     
 def paragraph16():
     
@@ -55,4 +54,4 @@ def paragraph16():
     
     story_2= f"On a Thursday the characters decided to teach each other instruments.{name_Jeremy}taught the trumpet.    {name_Janine} taught the piano.{name_John} taught the trombone.{name_Jane} taught the violin.{name_Jenny}taught      the cello.{name_Jack} taught the saxophone.The characters had great fun teaching eac oter." 
    
-   return(story_2)
+   return story_2
