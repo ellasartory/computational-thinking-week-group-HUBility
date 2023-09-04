3866a14840eb8a62530bb3c21e2374ac1c0ae4c0
i0ann4 <ioanna.leonidou21@gmail.com>
2023-09-04 16:48:45 +0200

    paragraph functions

--- challenge_day1/Ioanna.py
+++ challenge_day1/Ioanna.py
@@ -1,2 +1,41 @@
-msg = 'Ioanna'
-print(msg)
+def name():
+    name = 'Ioanna'
+    return name
+
+from Alicja import Jack
+from ella import Jenny
+from misha import Jeremy
+from cristian import John
+from vanessa import Jane
+
+def Janine():
+    name2 = 'Janine'
+    return name2
+print (janine())
+
+def paragraph_4():
+    name1 = jack()
+    name2 = jenny()
+    name3 = jeremy()
+    name4 = john()
+    name5 = jane()
+    paragraph = f'On a Thursday in the summer.{name1} played the trumpet.{name2} played the piano. {name3} played the trombone. Janine played the violin.{name4} played the cello.{name5} played the saxophone.'
+    return paragraph
+
+def paragraph_9():
+    name1 = jack()
+    name2 = jenny()
+    name3 = jeremy()
+    name4 = john()
+    name5 = jane()
+    paragraph = f'On Wednesday {name2} ate some oranges.{name3} decided to bake some cookies. Janine ate some nice french fries.{name4} had some nice cola. {name5} ate a good apple. {name1} had some expensive lobster.'
+    return paragraph
+
+def paragraph_14():
+    name1 = jack()
+    name2 = jenny()
+    name3 = jeremy()
+    name4 = john()
+    name5 = jane()
+    paragraph = f'Tuesday was a boring day for everyone so.... On Tuesday {name1} went to a bar. {name5} biked through town.{name4} went to a museum. Janine walked through the center. {name3} went to a cozy café. {name2} met her date at a Spanish restaurant.'
+    return paragraph
