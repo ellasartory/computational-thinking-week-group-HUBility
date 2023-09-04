2999b11eb24a5406a145ade1f91b3d01bfb85f6c
Mishanicho <misha.teunisse@gmail.com>
2023-09-04 16:32:16 +0200

    did it again

--- challenge_day1/misha.py
+++ challenge_day1/misha.py
@@ -1,21 +1,34 @@
+'''
 from Alicja import Jack
 from ella import Jenny
 from Vanessa import Jane
 from Cristian import John
 from Ioanna import Janine
-
+''' 
 
 def my_name():
     name= "Misha"
     return(name) 
 
-noun= my_name()
-print(noun)  
 
 def Jeremy():
    return "Jeremy"
+'''
 
 def paragraph_five():
-    story =  f"On a warm Friday in the summer, things didnt go so well: import Jack    broke his leg. " import John " lost his mum. " import Jenny " went through her back. "import Janine " could not be seen. " import Jeremy " had to go to therapy. And "import Jane" fell from a plane. Quite a bad day." 
+    name1 = jack()
+    name2 = Jenny()
+    name3 = Jane()
+    name4 = John()
+    name5 = Janine()
+    story =  'f/ On a warm Friday in the summer, things didnt go so well: {name1}   broke his leg. {name4} lost his mum. {name2} went through her back. {name5} could not be seen. Jeremy had to go to therapy. And {name3} fell from a plane. Quite a bad day.'
 
+def paragraph_ten():
+    story2 = f" On a Thursday in the autumn the characters tried each other’s instruments. " import Jenny " played the trumpet. " import Jack " played the piano. " import Janine " played the trombone. Jeremy played the violin. " import Jane " played the cello. " import John " played the saxophone. The characters had great fun trying each other’s instruments.
+    
+def paragraph_fiveteen():
+    story3 = f" On Wednesday "import John " ate some oranges. " import Janine " decided to bake some cookies. Jeremy ate some nice french fries. " import Jane " had some nice cola. " import Jack " ate a good apple. " import Jenny " had some expensive lobster.
+
+
+'''
 
