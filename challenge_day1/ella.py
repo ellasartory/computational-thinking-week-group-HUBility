78f0369ca4d64f62fea4808f7730d0e9b7debeac
Alicja Dorobis <alicja.dorobis@student.uva.nl>
2023-09-05 15:17:30 +0200

    Ella better v

--- challenge_day1/ella.py
+++ challenge_day1/ella.py
@@ -1,10 +1,9 @@
 
 #function to print own name
 def return_name():
-    name = "Ella"
-    return name
+    return "Ella"
 
-print(return_name())
+
 
 #function to return character name
 def Jenny():
@@ -15,18 +14,18 @@ def Jenny():
 #order of names needs to be the same here
 def paragraph3(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane):
 
-    story = f"On Wednesday in the summer. {name_Jack()} ate some oranges. {name_Jenny()} decided to bake some cookies. {name_Jeremy()} ate some nice french fries. {name_Janine()} had some nice cola. {name_John()} ate a good apple. {name_Jane()} had some expensiveaa looobster."
+    story = f"On Wednesday in the summer. {name_Jack} ate some oranges. {name_Jenny} decided to bake some cookies. {name_Jeremy} ate some nice french fries. {name_Janine} had some nice cola. {name_John} ate a good apple. {name_Jane} had some expensiveaa looobster."
     return story 
 
 
 def paragraph8(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane):
 
-    story = f"Tuesday was a fun autumn day for everyone so.... On Tuesday {name_Jane()} went to a bar. {name_Jack()} biked through town. {name_Jenny()} went to a museum. {name_Janine()} walked through the center. {name_Jeremy()} went to a cozy café. {name_John()} met his date at a Spani restauroont."
+    story = f"Tuesday was a fun autumn day for everyone so.... On Tuesday {name_Jane} went to a bar. {name_Jack} biked through town. {name_Jenny} went to a museum. {name_Janine} walked through the center. {name_Jeremy} went to a cozy café. {name_John} met his date at a Spani restauroont."
     return story 
 
 
 def paragraph13(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane):
 
-    story = f"It was winter. Monday was a regular day. On Monday {name_Jeremy()} went swimming. {name_Janine()} went to play soccer. {name_Jane()} went for a run. {name_John()} went canoeing. {name_Jenny()} Played tennis and {name_Jack()} was sick so he laid the entire day at beed."
+    story = f"It was winter. Monday was a regular day. On Monday {name_Jeremy} went swimming. {name_Janine} went to play soccer. {name_Jane} went for a run. {name_John} went canoeing. {name_Jenny} Played tennis and {name_Jack} was sick so he laid the entire day at beed."
 
     return story  
