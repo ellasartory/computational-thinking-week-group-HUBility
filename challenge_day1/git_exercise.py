5f7a2df0cdf3ee953b81f6308f3a000e43c9494a
i0ann4 <ioanna.leonidou21@gmail.com>
2023-09-05 12:36:13 +0200

    fixed some errors

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -13,19 +13,19 @@ from misha import Jeremy
 
 from Alicja import paragraph1
 from Cristian import paragraph2
-from Ella import paragraph3
+from ella import paragraph3
 from Ioanna import paragraph4
-from Misha import paragraph5
+from misha import paragraph5
 from Vanessa import paragraph6
 from Cristian import paragraph7
-from Ella import paragraph8
+from ella import paragraph8
 from Ioanna import paragraph9
-from Misha import paragraph10
+from misha import paragraph10
 from Vanessa import paragraph11
 from Alicja import paragraph12
-from Ella import paragraph13
+from ella import paragraph13
 from Ioanna import paragraph14
-from Misha import paragraph15
+from misha import paragraph15
 from Vanessa import paragraph16
 from Alicja import paragraph17
 from Cristian import paragraph18
