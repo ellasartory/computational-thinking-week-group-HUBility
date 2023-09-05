3b76052861d49dccf31f5badc6d63881ede453ec
i0ann4 <ioanna.leonidou21@gmail.com>
2023-09-05 14:39:04 +0200

    hashed out extra commands

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -41,29 +41,29 @@ print(Cristian.paragraph18(name_Jack, name_Jenny, name_Jeremy, name_Janine, name
 =======
 #import Ioanna, misha, Vanessa, ella, Alicja, Cristian
 
-from Alicja import Jack
-from ella import Jenny
-from Vanessa import Jane
-from Cristian import John
-from Ioanna import Janine
-from misha import Jeremy
+# from Alicja import Jack
+# from ella import Jenny
+# from Vanessa import Jane
+# from Cristian import John
+# from Ioanna import Janine
+# from misha import Jeremy
 
-from Alicja import paragraph1
-from Cristian import paragraph2
-from ella import paragraph3
-from Ioanna import paragraph4
-from misha import paragraph5
-from Vanessa import paragraph6
-from Cristian import paragraph7
-from ella import paragraph8
-from Ioanna import paragraph9
-from misha import paragraph10
-from Vanessa import paragraph11
-from Alicja import paragraph12
-from ella import paragraph13
-from Ioanna import paragraph14
-from misha import paragraph15
-from Vanessa import paragraph16
-from Alicja import paragraph17
-from Cristian import paragraph18
->>>>>>> 5f7a2df0cdf3ee953b81f6308f3a000e43c9494a
+# from Alicja import paragraph1
+# from Cristian import paragraph2
+# from ella import paragraph3
+# from Ioanna import paragraph4
+# from misha import paragraph5
+# from Vanessa import paragraph6
+# from Cristian import paragraph7
+# from ella import paragraph8
+# from Ioanna import paragraph9
+# from misha import paragraph10
+# from Vanessa import paragraph11
+# from Alicja import paragraph12
+# from ella import paragraph13
+# from Ioanna import paragraph14
+# from misha import paragraph15
+# from Vanessa import paragraph16
+# from Alicja import paragraph17
+# from Cristian import paragraph18
+# >>>>>>> 5f7a2df0cdf3ee953b81f6308f3a000e43c9494a
