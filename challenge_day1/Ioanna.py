c7bc1ed8b772e571b45231b61caf6210a6216ff8
Mishanicho <89473139+Mishanicho@users.noreply.github.com>
2023-09-05 15:39:18 +0200

    Update Ioanna.py

--- challenge_day1/Ioanna.py
+++ challenge_day1/Ioanna.py
@@ -7,11 +7,11 @@ def Janine():
     return "Janine"
     
 def paragraph4(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane):
-    paragraph = f'On a Thursday in the summer. {name_Jack} played the trumpet.{name_Jenny} played the piano. {name_Jeremy} played the trombone. Janine played the violin.{name_John} played the cello.{name_Jane} played tha sxxophone.'
+    paragraph = f'On a Thursday in the summer. {name_Jack} played the trumpet.{name_Jenny} played the piano. {name_Jeremy} played the trombone. Janine played the violin.{name_John} played the cello.{name_Jane} played the sxxophone.'
     return paragraph
     
 def paragraph9(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane):
-    paragraph = f'On Wednesday {name_Jenny} ate some oranges. {name_Jeremy} decided to bake some cookies. Janine ate some nice french fries. {name_John} had some nice cola. {name_Jane} ate a good apple. {name_Jack} had some expynsive lonster.'
+    paragraph = f'On Wednesday {name_Jenny} ate some oranges. {name_Jeremy} decided to bake some cookies. Janine ate some nice french fries. {name_John} had some nice cola. {name_Jane} ate a good apple. {name_Jack} had some expansive lonster.'
     return paragraph
 
 def paragraph14(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane):
