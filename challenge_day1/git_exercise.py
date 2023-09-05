750c74f524a1fcfef191b3e52db1cc5c1e00dd57
ellasartory <ella.sartory@student.uva.nl>
2023-09-05 14:35:52 +0200

    exercise

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -2,23 +2,38 @@ def teamname():
     return('This is Team HUBility. We are:')
 print(teamname())
 
-import Ioanna, misha, Vanessa, ella, Alicja, Cristian
+# Import all the modules
+import Ioanna
+import misha
+import Vanessa
+import ella
+import Alicja
+import Cristian
+import name_Jack
+import name_Jenny
+import name_Jeremy
+import name_Janine
+import name_John
+import name_Jane
 
-from Alicja import paragraph1
-from Cristian import paragraph2
-from Ella import paragraph3
-from Ioanna import paragraph4
-from Misha import paragraph5
-from Vanessa import paragraph6
-from Cristian import paragraph7
-from Ella import paragraph8
-from Ioanna import paragraph9
-from Misha import paragraph10
-from Vanessa import paragraph11
-from Alicja import paragraph12
-from Ella import paragraph13
-from Ioanna import paragraph14
-from Misha import paragraph15
-from Vanessa import paragraph16
-from Alicja import paragraph17
-from Cristian import paragraph18
+# Call each paragraph 
+
+print(Ioanna.paragraph1(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(Cristian.paragraph2(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(ella.paragraph3(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(Ioanna.paragraph4(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(misha.paragraph5(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(Vanessa.paragraph6(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(Cristian.paragraph7(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(ella.paragraph8(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(Ioanna.paragraph9(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(misha.paragraph10(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(Vanessa.paragraph11(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(Alicja.paragraph12(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(ella.paragraph13(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(Ioanna.paragraph14(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(misha.paragraph15(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(Vanessa.paragraph16(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(Alicja.paragraph17(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+print(Cristian.paragraph18(name_Jack, name_Jenny, name_Jeremy, name_Janine, name_John, name_Jane))
+
