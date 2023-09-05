0b14d8acf2a07877909e0ef461a3b66e2ed21dbb
i0ann4 <ioanna.leonidou21@gmail.com>
2023-09-05 15:34:39 +0200

    a

--- challenge_day1/Vanessa.py
+++ challenge_day1/Vanessa.py
@@ -6,20 +6,20 @@ def Jane():
     return "Jane"
 
 
-def paragraph6(name_jack,name_Jenny,name_Jeremy,name_Janine,name_John,name_Jane):
+def paragraph6(name_Jack,name_Jenny,name_Jeremy,name_Janine,name_John,name_Jane):
     
     story= f"On Saturday {name_Jenny} went out to party. {name_Jack} met a friend.{name_Janine} built a       treehouse.{name_John} worked at a cafe.{name_Jeremy} babysat his little sibling.{name_Jane} went or vacaton."
     
     return story
 
-def paragraph11(name_jack,name_Jenny,name_Jeremy,name_Janine,name_John,name_Jane):
+def paragraph11(name_Jack,name_Jenny,name_Jeremy,name_Janine,name_John,name_Jane):
    
 
     story_1= f"On a cold Friday in autumn, things were looking bleak:{name_John} broke his leg.{name_Jenny}lost her mum. went through her back. {name_Jeremy} could not be seen.{name_Jane} had to go to therapy.And {name_Jack}fell from a plane.{name_Janine}Quite a bar dey."
     
     return story_1
     
-def paragraph16(name_jack,name_Jenny,name_Jeremy,name_Janine,name_John,name_Jane):
+def paragraph16(name_Jack,name_Jenny,name_Jeremy,name_Janine,name_John,name_Jane):
     
     
     story_2= f"On a Thursday the characters decided to teach each other instruments.{name_Jeremy}taught the trumpet.{name_Janine} taught the piano.{name_John} taught the trombone.{name_Jane} taught the violin.{name_Jenny}taught the cello.{name_Jack} taught the saxophone.The characters had great fun teaching eac oter." 
