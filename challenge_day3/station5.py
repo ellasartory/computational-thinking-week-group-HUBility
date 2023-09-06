0465dcc7afac0d7ee2cdcdb952efd277e038a05c
ellasartory <ella.sartory@student.uva.nl>
2023-09-06 12:23:59 +0200

    edeit

--- challenge_day3/station5.py
+++ challenge_day3/station5.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+# Read the Excel file into a DataFrame without specifying column names
+dataframe1 = pd.read_excel("export_team_formation.xlsx", header=None)
+
+#print(dataframe1)
+
+#trying to access first item (name) and sixth item (team number) from each row 
+
+# Access the first and sixth items in each row
+first_item_list = dataframe1.apply(lambda row: row.str.split(',')[0] if ',' in row.values else '', axis=1)
+sixth_item_list = dataframe1.apply(lambda row: row.str.split(',')[5] if len(row) > 5 and ',' in row.values else '', axis=1)
+
+# Print the first and sixth items
+print("First Items:")
+print(first_item_list)
+
+print("Sixth Items:")
+print(sixth_item_list)
