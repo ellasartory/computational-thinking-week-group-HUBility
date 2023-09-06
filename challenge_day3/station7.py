f497da4a36955f446af6fb055e56cc5e21d65e7e
Cristian2406 <cristian.tomus@student.uva.nl>
2023-09-06 11:56:59 +0200

    Version2

--- challenge_day3/station7.py
+++ challenge_day3/station7.py
@@ -1,3 +1,4 @@
+from sympy import symbols, Eq, solve
 import numpy as np
 
 def solution_station_7(expressions, known_vars):
@@ -14,7 +15,7 @@ def solution_station_7(expressions, known_vars):
     result_1 = solution[0] + initial_guess[1]
     result_2 = initial_guess[0] + initial_guess[2] * solution[0] + initial_guess[4]
 
-    return solution_station_7
+    return solution
 
 # Define the input equations (in terms of symbols a, b, c, d, e)
 a, b, c, d, e = symbols('a b c d e')
@@ -30,11 +31,11 @@ equations = [
 known_variables = [a, b, c, d, e]
 
 # Calculate the values of expressions using the custom solver
-solution_station_7 = solve_equations(equations, known_variables)
+solution = solution_station_7(equations, known_variables)
 
-print (str.(a))
-print (str.(b))
-print (str.(c))
-print (str.(d))
-print (str.(e))
-
+# Print the values of the variables
+print("a =", solution[0])
+print("b =", solution[1])
+print("c =", solution[2])
+print("d =", solution[3])
+print("e =", solution[4])
