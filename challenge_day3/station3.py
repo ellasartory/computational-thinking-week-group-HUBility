21a1a207182a208521654f40dbec58ca8465a88e
Mishanicho <89473139+Mishanicho@users.noreply.github.com>
2023-09-06 12:31:12 +0200

    Update station3.py

--- challenge_day3/station3.py
+++ challenge_day3/station3.py
@@ -1,9 +1,2 @@
 def solution_station_3(number):
     return number % 3 == 0
-
-# Test cases
-print(solution_station_3(4))   
-print(solution_station_3(12))   
-print(solution_station_3(3969))  
-print(solution_station_3(0))  
-
