18430ad18e8571695c5893ac8f572444ea67f81a
ellasartory <ella.sartory@student.uva.nl>
2023-09-06 11:30:41 +0200

    station1

--- challenge_day3/station1.py
+++ challenge_day3/station1.py
@@ -1,1 +1,23 @@
 
+#Fibonacci numbers
+def solution_station_1(n):
+ 
+    # Check if input is 0 
+    if n < 0:
+        print("Input not possible")
+ 
+    # Check if n is 0
+    elif n == 0:
+        return 0
+ 
+    # Check if n is 1,2
+    elif n == 1 or n == 2:
+        return 1
+ 
+    else:
+        return solution_station_1(n-1) + solution_station_1(n-2)
+ 
+ 
+#insert number
+number = x
+print(solution_station_1(x))
