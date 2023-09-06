6476ecf278a426b4df4743fd13bf6748b651269f
alicja-a-d <113539951+alicja-a-d@users.noreply.github.com>
2023-09-06 14:00:13 +0200

    Update station1.py

--- challenge_day3/station1.py
+++ challenge_day3/station1.py
@@ -1,18 +1,18 @@
 
-#Fibonacci numbers
+#Fibonacci numbers sequence
 def solution_station_1(n):
-    if n <= 0:
-        return []
-
-    sequence = [0, 1]  # Initialize the sequence with the first two Fibonacci numbers
-
-    if n <= 1:
-        return sequence[:n + 1]
-
+    if n < 0:
+        raise ValueError
+    
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    
     a, b = 0, 1
     for _ in range(2, n + 1):
         next_fib = a + b
-        sequence.append(next_fib)
         a, b = b, next_fib
 
-    return sequence
+    return b
+
