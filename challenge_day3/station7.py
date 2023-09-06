990fb412c3518c06ec7bcba1783869acaeb23c99
Mishanicho <89473139+Mishanicho@users.noreply.github.com>
2023-09-06 14:48:46 +0200

    Update station7.py

--- challenge_day3/station7.py
+++ challenge_day3/station7.py
@@ -1,30 +1,28 @@
-import numpy as np
-from scipy.optimize import root
-
 def solution_station_7(expressions, known_vars):
-    def equations_to_solve(x):
-        return [
-            x[0] + x[1] + x[2] + x[3] - 13,
-            x[3] * x[2] - 28,
-            x[3] * x[0] + x[2] * x[1] - 17,
-            x[3] * x[0] * x[2] * x[4] - 42,
-            x[4] + x[3] + x[1] * x[2] - 3.5
-        ]
+    # Define the values of variables a, b, c, d, and e
+    variables = {'a': 3, 'b': -1, 'c': 4, 'd': 7, 'e': 0.5}
 
-    # Initialize the initial guess for the unknowns
-    initial_guess = np.array([1.0, 1.0, 1.0, 1.0, 1.0])
+    results = {}
+    
+    for expression in expressions:
+        try:
+            result = eval(expression, variables)
+            results[expression] = result
+        except Exception as e:
+            print(f"Error evaluating {expression}: {e}")
+            results[expression] = None
 
-    # Use a numerical solver to find the values of the unknowns
-    solution = root(equations_to_solve, initial_guess)
-
-    return solution.x
+    return results
 
 # Define the list of known variables
 known_variables = ['a', 'b', 'c', 'd', 'e']
 
-# Calculate the values of expressions using the custom solver
-solution = solution_station_7([], known_variables)
+# Define the list of expressions to be evaluated
+expressions = ['a * b + c', 'd - e', 'c / d']
 
-# Print the values of the variables
-for var, value in zip(known_variables, solution):
-    print(f"{var} =", value)
+# Calculate the values of expressions using the custom solver
+results = solution_station_7(expressions, known_variables)
+
+# Print the results
+for expression, result in results.items():
+    print(f"Result of {expression}: {result}")
