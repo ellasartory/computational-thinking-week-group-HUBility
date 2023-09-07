c6dfcbe78547b543f81e46925b02fd514163b1b0
Ella Sartory <126684201+ellasartory@users.noreply.github.com>
2023-09-07 15:22:41 +0200

    Update station7.py

--- challenge_day3/station7.py
+++ challenge_day3/station7.py
@@ -7,7 +7,7 @@ e = 0.5
 def solution_station_7(expression):
     try:
         result = eval(expression)
-        result = int(result)
+        result = float(result)
         return result
     except Exception as e:
         print(f"Error: {e}")
