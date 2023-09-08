4a20b650456897cf5688cb81cadf15919f51d0b1
Mishanicho <misha.teunisse@gmail.com>
2023-09-08 14:17:05 +0200

    Change for station4 of Challenge day 3 - 14:17 Friday

--- challenge_day3/station4.py
+++ challenge_day3/station4.py
@@ -1,5 +1,8 @@
 def solution_station_4(n):
-    for i in range(2, int(n/2) + 1):
-        if (n%i) == 0:
-            return False
-    return True
+    if n > 1:
+        for i in range(2, int(n/2)+1):
+            if (n % i) == 0:
+                return False
+        return True
+    else: 
+        return False
